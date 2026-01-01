305e53b74d0983e99daadfc8ed67cb0363faaa1d
Aniket Shrivastava <83110735+iamaniket-python@users.noreply.github.com>
2026-01-01 21:33:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def welcome():
     return "Welcome"
 
 students=[
-    Student(roll_no =1, name="Aniket srivastava",student_class=2, age= 14, gender= "Male", phone= "123456789")
+    Student(roll_no =1, name="Aniket srivastava",student_class=11, age= 14, gender= "Male", phone= "123456789")
 ]
 
 @app.get("/students")
